8c89367838dc16dda08e0a2ec729cdeb93b6c545
jeanschwalenberg <jeanschwalenberg@gmail.com>
2016-01-29 22:01:05 -0700

    + FlexibleDiscountHelper.cs
    conditional binding

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -26,6 +26,7 @@ namespace EssentialTools.Infrastructure {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
             //kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
             kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>().WhenInjectedInto<LinqValueCalculator>();
         }
     }
 }
